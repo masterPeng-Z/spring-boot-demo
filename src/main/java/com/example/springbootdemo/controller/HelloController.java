d7c642d308c731f474ad27d58f24f33297f2e977
peng <18179750192@163.com>
2019-12-18 23:10:58 +0800

    新增nacos作为配置中心

--- src/main/java/com/example/springbootdemo/controller/HelloController.java
+++ src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.springbootdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Administrator
  * @since 2019/12/12 15:22
  */
-
+@RefreshScope
 @RestController
 public class HelloController {
-    @RequestMapping("/") String index() { return "Hello Spring Boot"; }
+    @Value("${configString}")
+    private String configString;
+
+    @RequestMapping("/") String index() { return configString; }
 }
